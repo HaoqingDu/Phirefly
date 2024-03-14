1b4f966c955bb670be590faae191fd1d79ab926c
HaoqingDu <duhaoqing115@gmail.com>
2024-03-14 15:06:00 +0100

    create function "loglikelihood"

---
+++ R/function1.R
@@ -0,0 +1,13 @@
+#' Calculate the likelihood
+#'
+#' @param phy phylogeny
+#' @param chr.values continuous trait values
+#' @param par parameter vectors
+#'
+#' @return log-likelihood
+#' @export
+#'
+#' @examples
+loglikelihood <- function(phy, chr.values, par){
+  return(0)
+}
