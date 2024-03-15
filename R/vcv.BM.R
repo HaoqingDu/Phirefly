c51d1bea46a4c4b4ca7609e93e3863b8ec71db0d
HaoqingDu <duhaoqing115@gmail.com>
2024-03-15 14:44:46 +0100

    complete function "vcv.BM"

--- R/vcv.BM.R
+++ R/vcv.BM.R
@@ -6,31 +6,31 @@
 
 vcv.BM <- function(phy){
   if(class(phy) != "phylo") stop(phy," is not a \" phylo \".")
-  common.ancestor <- function(i,j){
+  common.ancestor <- function(i,j){ # function to find CA of taxa i & j
     if (phy$edge[phy$edge[,2]==i,1]
-        == phy$edge[tree$edge[,2]==j,1]) {
-      return(tree$edge[tree$edge[,2]==i,1])
+        == phy$edge[phy$edge[,2]==j,1]) {
+      return(phy$edge[phy$edge[,2]==i,1])
     }
-    else if (tree$edge[tree$edge[,2]==i,1]>tree$edge[tree$edge[,2]==j,1]) {
-      return(common.ancestor(tree$edge[tree$edge[,2]==i,1],j))
+    else if (phy$edge[phy$edge[,2]==i,1]>phy$edge[phy$edge[,2]==j,1]) {
+      return(common.ancestor(phy$edge[phy$edge[,2]==i,1],j))
     }
     else {
-      return(common.ancestor(i,tree$edge[tree$edge[,2]==j,1]))
+      return(common.ancestor(i,phy$edge[phy$edge[,2]==j,1]))
     }
   }
 
-  from.root <- function(node){
-    if (node == length(tree$tip.label)+1) {
+  from.root <- function(node){ # the total branch length from the root to certain node
+    if (node == length(phy$tip.label)+1) {
       return(0)
     } else{
-      return(tree$edge.length[tree$edge[,2]==node]+from.root(tree$edge[tree$edge[,2]==node,1]))
+      return(phy$edge.length[phy$edge[,2]==node]+from.root(phy$edge[phy$edge[,2]==node,1]))
     }
   }
-  for (i in 1:length(tree$tip.label)) {
-    tree$edge[tree$edge[,2]==i,1]
+  for (i in 1:length(phy$tip.label)) {
+    phy$edge[phy$edge[,2]==i,1]
   }
 
-  C <- matrix(NA, nrow = length(tree$tip.label), ncol = length(tree$tip.label))
+  C <- matrix(NA, nrow = length(phy$tip.label), ncol = length(phy$tip.label))
 
   for (n in 1:ncol(C)) {
     for (m in 1:n) {
@@ -38,7 +38,7 @@ vcv.BM <- function(phy){
         C[m,n] <- from.root(common.ancestor(m,n))
         C[n,m] <- C[m,n]
       } else {
-        C[m,n] <- from.root(tree$edge[tree$edge[,2] == m,1]) + tree$edge.length[tree$edge[,2] == m]
+        C[m,n] <- from.root(phy$edge[phy$edge[,2] == m,1]) + phy$edge.length[phy$edge[,2] == m]
       }
     }
   }
