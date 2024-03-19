117fb96a4f8c98710e768c00f5f3dc7e15897da9
HaoqingDu <duhaoqing115@gmail.com>
2024-03-19 17:11:50 +0100

    complete 'Loglike.BM' script with ancestral values, variance values, and likelihood function

--- R/vcv.R
+++ R/vcv.R
@@ -80,7 +80,7 @@ vcv.BM <- function(phy){
 vcv.OU <- function(phy, sigma2, adpt.rate){
   if(class(phy) != "phylo") stop(phy," is not a \" phylo \".")
   C <- matrix(NA, nrow = length(phy$tip.label), ncol = length(phy$tip.label))
-    for (n in 1:ncol(C)) {
+  for (n in 1:ncol(C)) {
     for (m in 1:n) {
       C[m,n] <- sigma2/(2*adpt.rate)*
         exp(-adpt.rate*dist(phy, m, n))*
@@ -88,4 +88,5 @@ vcv.OU <- function(phy, sigma2, adpt.rate){
       C[n,m] <- C[m,n]
     }
   }
+  return(C)
 }
