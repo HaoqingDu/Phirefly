89727470cfefd0b7a3c0311747e3acdb066cab0a
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-03-20 11:03:55 +0100

    fix gls

--- R/generalized_least_squares.R
+++ R/generalized_least_squares.R
@@ -2,6 +2,8 @@
 #'
 #' @param V residual among-species covariance matrix
 #' @param X model matrix
+#' @param y the observations
+#' @param y the parameter vector
 #'
 #' @return the log likelihood value
 #' @export
@@ -10,16 +12,19 @@
 #'
 #' V <- crossprod(matrix(rnorm(16),4,4))
 #' X <- matrix(1,4,1)
+#' y <- c(0.1, 0.3, -0.3, 0.0)
+#' beta1 <- 0.2
 #'
-#' generalized_least_squares(V, X)
-generalized_least_squares <- function(V, X){
+#' generalized_least_squares(V, X, y, beta1)
+generalized_least_squares <- function(V, X, y, beta1){
   ntip <- nrow(V)
   L = t(chol(V)) # lower triangular matrix
 
   log_det_V <- 2*sum(log(diag(L)))
 
-  r = solve(L) %*% y - solve(L) %*% X * theta # what does de-correlated residuals mean?
+  r = solve(L) %*% y - solve(L) %*% X %*% beta1 # what does de-correlated residuals mean?
 
-  logl = -0.5 * ntip * log(2*pi) -0.5 * log_det_V - 0.5 * dot(r, r)
+  dot_r <- (t(r) %*% r)[1]
+  logl = -0.5 * ntip * log(2*pi) -0.5 * log_det_V - 0.5 * dot_r
   return(logl)
 }
