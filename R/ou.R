1865e5545baa96a01bbfd34153b161176be19fc9
Priscilla Lau <prilau@Priscillas-Laptop.local>
2024-03-21 13:35:56 +0100

    ou likelihood

---
+++ R/ou.R
@@ -0,0 +1,241 @@
+library(ape)
+library(phytools)
+library(pracma)
+
+# Pruning algorithm
+
+# Postorder function
+postorder <- function(node_index, edge, tree, continuousChar,
+                      μ, V, log_norm_factor, branch_lengths, alpha, sigma2, theta){
+  ntip = length(tree$tip.label)
+
+  # if is internal node
+  if (node_index > ntip){
+
+    left_edge  = which(edge[,1] == node_index)[1] # index of left child edge
+    right_edge = which(edge[,1] == node_index)[2] # index of right child edge
+    left = edge[left_edge,2] # index of left child node
+    right = edge[right_edge,2] # index of right child node
+
+    output_left <- postorder(left, edge, tree, continuousChar,
+                             μ, V, log_norm_factor, branch_lengths, alpha, sigma2, theta)
+    μ <- output_left[[1]]
+    V <- output_left[[2]]
+    log_norm_factor <- output_left[[3]]
+
+    output_right <- postorder(right, edge, tree, continuousChar,
+                              μ, V, log_norm_factor, branch_lengths, alpha, sigma2, theta)
+    μ <- output_right[[1]]
+    V <- output_right[[2]]
+    log_norm_factor <- output_right[[3]]
+
+
+    bl_left = branch_lengths[left_edge] # all branch of left child edge
+    bl_right = branch_lengths[right_edge] # all branch of right child edge
+
+    # 1) variance of the normal variable: this branch (v_left) and the subtree (V[left])
+
+    v_left = sigma2/(2*alpha) *expm1(2.0*alpha*bl_left)
+    var_left = v_left + V[left] * exp(2.0 * alpha * bl_left)
+
+    v_right = sigma2/(2*alpha) *expm1(2.0*alpha*bl_right)
+    var_right = v_right + V[right] * exp(2.0 * alpha * bl_right)
+
+    # 2) mean of the normal variable
+    mean_left = exp(alpha*bl_left)*(μ[left] - theta) + theta
+    mean_right = exp(alpha*bl_right)*(μ[right] - theta) + theta
+
+    ## compute the mean and variance of the node
+    mean_ancestor = (mean_left * var_right + mean_right * var_left) / (var_left + var_right)
+    μ[node_index] = mean_ancestor
+    var_node = (var_left * var_right) / (var_left + var_right)
+    V[node_index] = var_node
+
+    ## compute the normalizing factor, the left-hand side of the pdf of the normal variable
+    log_nf_left = bl_left * alpha
+    log_nf_right = bl_right * alpha
+
+    contrast = mean_left - mean_right
+    a = -(contrast*contrast / (2*(var_left+var_right)))
+    b = log(2*pi*(var_left+var_right))/2.0
+    #b = log(2*pi)/2.0 + log(var_left+var_right)/2.0
+    log_nf = log_nf_left + log_nf_right + a - b
+    log_norm_factor[node_index] = log_nf
+
+
+    return(list(μ, V, log_norm_factor))
+  }
+
+
+  # if is tip
+  else{
+    species = tree$tip.label[node_index]
+
+    μ[node_index] = as.numeric(continuousChar[[which(names(continuousChar) == species)]])
+    V[node_index] = 0.0 ## if there is no observation error
+
+    return(list(μ, V, log_norm_factor))
+  }
+}
+
+## log-likelihood + root treatment
+simple_ou_pruning <- function(tree, continuousChar, alpha, sigma2, theta){
+  ntip = length(tree$tip.label) # number of tips
+  edge = tree$edge # equals tree[:edge] in Julia
+  n_edges = length(edge[,1]) # number of edges
+  max_node_index = max(tree$edge) # total number of nodes
+
+  V = numeric(max_node_index)
+  μ = numeric(max_node_index)
+  log_norm_factor = numeric(max_node_index)
+
+  branch_lengths = tree$edge.length
+
+  root_index = ntip + 1
+
+  output <- postorder(root_index, edge, tree, continuousChar,
+                      μ, V, log_norm_factor, branch_lengths, alpha, sigma2, theta)
+  μ <- output[[1]]
+  V <- output[[2]]
+  log_norm_factor <- output[[3]]
+
+  ## assume root value equal to theta
+  μ_root = μ[root_index]
+  v_root = V[root_index]
+  lnl = dnorm(theta, mean = μ_root, sd = sqrt(v_root), log = TRUE) # are \theta and \mu in correct positions?
+
+  ## add norm factor
+  for (log_nf in log_norm_factor){
+    lnl = lnl + log_nf
+  }
+  return(lnl)
+}
+
+# Generalised least square method (aka variance-covariance matrix method)
+simple_ou_vcv <- function(tree, continuousChar, alpha, sigma2, theta){
+  ntip <- length(tree$tip.label)
+  mrca1 <- ape::mrca(tree) # get the ancestral node label for each pair of tips
+  times <- ape::node.depth.edgelength(tree) # get time at each node from root
+  ta <- matrix(times[mrca1], nrow=ntip, dimnames = list(tree$tip.label, tree$tip.label)) # get time of divergence for each pair of tips
+  T.term <- times[1:ntip] # get time at tips
+  tia <- times[1:ntip] - ta
+  tja <- t(tia)
+  tij <- tja + tia # distance in time unit between two tips
+
+  vy = sigma2 / (2*alpha)
+
+  #V = vy * (1 - exp(-2 * alpha * ta)) * exp(-alpha * tij)
+  V = vy * -1 * expm1(-2 * alpha * ta) * exp(-alpha * tij) ### ta = time tgt; tij = time not tgt (sum of two branches)
+
+  X = matrix(1, ntip)
+
+  C = chol(V) # upper triangular matrix
+  L = t(C) # lower triangular matrix
+  log_det_V = 0
+  for (i in 1:ntip){
+    log_det_V = log_det_V + log(L[i,i])
+  }
+  log_det_V = log_det_V *2.0 # equals to julia implementation to 12 sig. fig.
+
+  y = NULL
+  for (species in tree$tip.label){
+    y[species] = as.numeric(continuousChar[species])
+  }
+
+  r = solve(L) %*% y - solve(L) %*% X * theta # what does de-correlated residuals mean?
+
+  # res = - (n/2) * log(2*pi) - 0.5 * log_det_V - 0.5 * dot(r, r)
+  #     = exp(-n/2)^(2*pi) * exp(-0.5)^det_V * exp(-0.5)^dot(r, r) ?
+  res = 0.0
+  res = res - 0.5 * ntip * log(2*pi)
+  res = res - 0.5 * log_det_V
+  res = res - 0.5 * dot(r, r) # what is r and what is  dot product of r?
+
+  return(res)
+}
+
+
+# multiple continuous characters
+# continuousChar. alpha, sigma2, and theta should be vectors of same length
+#multiChar.ou_pruning <- function(tree, continuousChar, alpha, sigma2, theta){
+#  sum_likelihood = 0
+#  for (i in 1:length(continuousChar)){
+#    sum_likelihood = sum_likelihood + simple_ou_pruning(tree, continuousChar[i], alpha[i], sigma2[i], theta[i])
+#  }
+#  return(sum_likelihood)
+#}
+
+
+#multiSample.postorder <- function(node_index, edge, tree, continuousChar,
+#                      μ, V, log_norm_factor, branch_lengths, alpha, sigma2, theta){
+#  ntip = length(tree$tip.label)
+#
+#  # if is internal node
+#  if (node_index > ntip){
+#
+#    left_edge  = which(edge[,1] == node_index)[1] # index of left child edge
+#    right_edge = which(edge[,1] == node_index)[2] # index of right child edge
+#    left = edge[left_edge,2] # index of left child node
+#    right = edge[right_edge,2] # index of right child node
+#
+#    output_left <- postorder(left, edge, tree, continuousChar,
+#                             μ, V, log_norm_factor, branch_lengths, alpha, sigma2, theta)
+#    μ <- output_left[[1]]
+#    V <- output_left[[2]]
+#    log_norm_factor <- output_left[[3]]
+#
+#    output_right <- postorder(right, edge, tree, continuousChar,
+#                              μ, V, log_norm_factor, branch_lengths, alpha, sigma2, theta)
+#    μ <- output_right[[1]]
+#    V <- output_right[[2]]
+#    log_norm_factor <- output_right[[3]]
+#
+#
+#    bl_left = branch_lengths[left_edge] # all branch of left child edge
+#    bl_right = branch_lengths[right_edge] # all branch of right child edge
+#
+#    # 1) variance of the normal variable: this branch (v_left) and the subtree (V[left])
+#
+#    v_left = sigma2/(2*alpha) *expm1(2.0*alpha*bl_left)
+#    var_left = v_left + V[left] * exp(2.0 * alpha * bl_left)
+#
+#    v_right = sigma2/(2*alpha) *expm1(2.0*alpha*bl_right)
+#    var_right = v_right + V[right] * exp(2.0 * alpha * bl_right)
+#
+#    # 2) mean of the normal variable
+#    mean_left = exp(alpha*bl_left)*(μ[left] - theta) + theta
+#    mean_right = exp(alpha*bl_right)*(μ[right] - theta) + theta
+#
+#    ## compute the mean and variance of the node
+#    mean_ancestor = (mean_left * var_right + mean_right * var_left) / (var_left + var_right)
+#    μ[node_index] = mean_ancestor
+#    var_node = (var_left * var_right) / (var_left + var_right)
+#    V[node_index] = var_node
+#
+#    ## compute the normalizing factor, the left-hand side of the pdf of the normal variable
+#    log_nf_left = bl_left * alpha
+#    log_nf_right = bl_right * alpha
+#
+#    contrast = mean_left - mean_right
+#    a = -(contrast*contrast / (2*(var_left+var_right)))
+#    b = log(2*pi*(var_left+var_right))/2.0
+#    #b = log(2*pi)/2.0 + log(var_left+var_right)/2.0
+#    log_nf = log_nf_left + log_nf_right + a - b
+#    log_norm_factor[node_index] = log_nf
+#
+#
+#    return(list(μ, V, log_norm_factor))
+#  }
+#
+#
+#  # if is tip
+#  else{
+#    species = tree$tip.label[node_index]
+#
+#    μ[node_index] = as.numeric(continuousChar[[which(names(continuousChar) == species)]])
+#    V[node_index] = 0.0 ## if there is no observation error
+#
+#    return(list(μ, V, log_norm_factor))
+#  }
+#}
+#multiSample.ou_pruning <-
---
+++ R/readwrite.R
@@ -0,0 +1,26 @@
+library(ape)
+library(phytools)
+library(tidyverse)
+
+extractContinuousChar <- function(phy, metadata, col_taxon, cols_contChars){
+  data <- read.csv(metadata)
+  tree <- read.tree(phy)
+
+  taxa_present <- intersect(tree$tip.label, data[[col_taxon]])
+
+
+
+  match_tree <- drop.tip(tree, tree$tip.label[-match(taxa_present, tree$tip.label)])
+  match_contChars <- data %>%
+    select(col_taxon, all_of(cols_contChars)) %>%
+    filter()
+
+
+  rownames(contChars) <- data[[col_taxon]]
+
+
+
+  return(c(match_tree, match_contChars)
+}
+
+
