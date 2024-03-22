62fa5b6ce8a3d732063d13fa99e0eddeb2afe3d5
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-03-22 10:37:01 +0100

    move vcv code into other file

--- R/OU_fitzjohn.R
+++ R/OU_fitzjohn.R
@@ -106,49 +106,6 @@ simple_ou_pruning <- function(tree, continuousChar, alpha, sigma2, theta){
   return(lnl)
 }
 
-# Generalised least square method (aka variance-covariance matrix method)
-simple_ou_vcv <- function(tree, continuousChar, alpha, sigma2, theta){
-  ntip <- length(tree$tip.label)
-  mrca1 <- ape::mrca(tree) # get the ancestral node label for each pair of tips
-  times <- ape::node.depth.edgelength(tree) # get time at each node from root
-  ta <- matrix(times[mrca1], nrow=ntip, dimnames = list(tree$tip.label, tree$tip.label)) # get time of divergence for each pair of tips
-  T.term <- times[1:ntip] # get time at tips
-  tia <- times[1:ntip] - ta
-  tja <- t(tia)
-  tij <- tja + tia # distance in time unit between two tips
-
-  vy = sigma2 / (2*alpha)
-
-  #V = vy * (1 - exp(-2 * alpha * ta)) * exp(-alpha * tij)
-  V = vy * -1 * expm1(-2 * alpha * ta) * exp(-alpha * tij) ### ta = time tgt; tij = time not tgt (sum of two branches)
-
-  X = matrix(1, ntip)
-
-  C = chol(V) # upper triangular matrix
-  L = t(C) # lower triangular matrix
-  log_det_V = 0
-  for (i in 1:ntip){
-    log_det_V = log_det_V + log(L[i,i])
-  }
-  log_det_V = log_det_V *2.0 # equals to julia implementation to 12 sig. fig.
-
-  y = NULL
-  for (species in tree$tip.label){
-    y[species] = as.numeric(continuousChar[species])
-  }
-
-  r = solve(L) %*% y - solve(L) %*% X * theta # what does de-correlated residuals mean?
-
-  # res = - (n/2) * log(2*pi) - 0.5 * log_det_V - 0.5 * dot(r, r)
-  #     = exp(-n/2)^(2*pi) * exp(-0.5)^det_V * exp(-0.5)^dot(r, r) ?
-  res = 0.0
-  res = res - 0.5 * ntip * log(2*pi)
-  res = res - 0.5 * log_det_V
-  res = res - 0.5 * dot(r, r) # what is r and what is  dot product of r?
-
-  return(res)
-}
-
 
 # multiple continuous characters
 # continuousChar. alpha, sigma2, and theta should be vectors of same length
