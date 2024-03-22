6a3884d363ee7e4831cd7a754711430ebb06956b
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-03-22 10:27:19 +0100

    can't call "library(x)" in the R source files, need to specify the name space instead

--- R/readwrite.R
+++ R/readwrite.R
@@ -1,19 +1,13 @@
-library(ape)
-library(phytools)
-library(tidyverse)
-
 extractContinuousChar <- function(phy, metadata, col_taxon, cols_contChars){
   data <- read.csv(metadata)
-  tree <- read.tree(phy)
+  tree <- ape::read.tree(phy)
 
-  taxa_present <- intersect(tree$tip.label, data[[col_taxon]])
+  taxa_present <- dplyr::intersect(tree$tip.label, data[[col_taxon]])
 
-
-
-  match_tree <- drop.tip(tree, tree$tip.label[-match(taxa_present, tree$tip.label)])
+  match_tree <- ape::drop.tip(tree, tree$tip.label[-match(taxa_present, tree$tip.label)])
   match_contChars <- data %>%
-    select(col_taxon, all_of(cols_contChars)) %>%
-    filter()
+    dplyr::select(col_taxon, all_of(cols_contChars)) %>%
+    dplyr::filter() ## @priscilla: something missing here?
 
   rownames(contChars) <- data[[col_taxon]]
 
