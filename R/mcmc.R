57beb6697ba49de53888e20c617d4fbb975b3d0f
HaoqingDu <duhaoqing115@gmail.com>
2024-05-06 16:35:05 +0200

    create mcmc scripts

---
+++ R/mcmc.R
@@ -0,0 +1,12 @@
+# Author: Haoqing Du
+# Latest Editing Time: 06/05/2024
+
+## Markov Chain Monte Carlo
+
+mcmc <- function(LikelihoodFunction,
+                 Priors,
+                 Parameters,
+                 LogTransforms,
+){
+
+}
